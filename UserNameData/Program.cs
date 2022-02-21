729cad1feb692c4084a060a1d0c97b70e6b95a3b
Keit Nõmme <knomme@tktk.ee>
2022-02-21 12:02:46 +0200

    Funktsions

---
+++ UserNameData/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UserNameData
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Programm palub kasutajal sisestada eesnime;
+			// Programm kuvab kasutaja eesnime pikkuse;
+			// Programm kuvab kasutaja eesnime esimese tähe;
+			// Programm kuvab kasutaja eesnime tagurpidi;
+			// Main meetodis ei tohi olla rohkem kui kolm rida koodi;
+
+			Console.WriteLine("Palun sisesta eesnimi:");
+			string userName = Console.ReadLine();
+
+			GetUserNameData(userName);
+		}
+		public static void GetUserNameData(string userInput)
+		{
+			Console.WriteLine($"Sinu nimi on {userInput.Length} sümbolit pikk");
+			Console.WriteLine($"Sinu eesnimne esimene täht on {userInput[0]}");
+
+			for (int i = userInput.Length - 1; i >= 0; i--)
+			{
+				Console.Write(userInput[i]);
+			}
+		}
+	}
+}
---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Programm palub kastuajal sisestada eesnime;
+			// Programm palun kastuajal sisestada number 1-3;
+			// Kui kastuaja sisestab 1, siis kuvab kastuaja eesnime tagurpidi;
+			// Kui kastuaja sisestab 2, siis kuvab kastuaja eesnime esimese tähe;
+			// Kui kastuaja sisestab 3, siis kuvab kastuaja eesnime pikkuse;
+
+			Console.WriteLine("Palun sisesta eesnimi:");
+			string userName = Console.ReadLine();
+
+			bool userLoop = true;
+
+			while (userLoop)
+			{
+				Console.WriteLine("Palun sisesta arv 1-3:");
+				int userNumber = Convert.ToInt32(Console.ReadLine());
+
+				if (userNumber == 1)
+				{
+					GetUserNameReverse(userName);
+					break;
+				}
+				else if (userNumber == 2)
+				{
+					GetUserNameFirst(userName);
+					break;
+				}
+				else if (userNumber == 3)
+				{
+					GetUserNameLength(userName);
+					break;
+				}
+				else
+				{
+					Console.WriteLine("Vale valik, Proovi uuesti!");
+				}
+			}
+
+		}
+		public static void GetUserNameReverse(string userInput)
+		{
+			for (int i = userInput.Length - 1; i >= 0; i--)
+			{
+				Console.Write(userInput[i]);
+			}
+		}
+
+		public static void GetUserNameFirst(string userInput)
+		{
+
+			Console.WriteLine($"Sinu eesnimne esimene täht on {userInput[0]}");
+		}
+
+		public static void GetUserNameLength(string userInput)
+		{
+			Console.WriteLine($"Sinu nimi on {userInput.Length} sümbolit pikk");
+		}
+	}
+
+}	
